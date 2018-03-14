7465b68aea24a1db4e8044358d2373b4b065dc89
Anish <anishkap7010@gmail.com>
2018-03-14 18:41:49 +0530

    Working till step 7 routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './all-heroes';
+import { MessageService } from './message.service';
+//getting data from the server(not from a mock file)
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService : MessageService) { }
 
-  getHeroes() : Hero[] {
-    return HEROES;
+  getHeroes() : Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+  
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,26 +7,19 @@ import { HeroService } from '../hero.service'
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  
-  heroes : Hero[];
+  heroes: Hero[];
 
-  selectedHero : Hero;
-  
-  onSelect(hero : Hero) : void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-  this.heroes = this.heroService.getHeroes();
-}
-
-  constructor(private heroService : HeroService) {
-
-   }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
 }
